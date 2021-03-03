eac67ee8159cb1d2ce669b59c7522bceadd3b486
gbloch <gaetan.bloch@gmail.com>
2021-03-03 17:59:22 +0100

    Initialize Spring boot project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-properties
+
 External properties with Spring Boot Framework
---
+++ src/test/java/com/gbloch/properties/SpringPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbloch.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
